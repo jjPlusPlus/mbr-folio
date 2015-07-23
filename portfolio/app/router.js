8b63e1760d58ec0f37a890b566f020abffe24df9
JJ Medina <jj.medina@runtriz.com>
2015-07-22 21:37:24 -0700

    initial setup of the router

--- portfolio/app/router.js
+++ portfolio/app/router.js
@@ -6,6 +6,30 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('projects', function() {
+    this.route('project');
+    this.route('new');
+    this.route('edit');
+  });
+  this.resource('posts', function() {
+    this.route('post');
+    this.route('new');
+    this.route('edit');
+  });
+  this.resource('events', function() {
+    this.route('new');
+    this.route('edit');
+  });
+  this.resource('experiences', function() {
+    this.route('new');
+    this.route('edit');
+  });
+
+  this.route('about');
+  this.route('images');
+  this.route('style');
+  this.route('admin');
+  this.route('analytics');
 });
 
 export default Router;
